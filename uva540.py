d470b441c7d11a8b1ff33f6d1de00e739f748e7e
0x6c6868 <1023347025@qq.com>
2017-11-09 22:41:55 +0800

    Create uva540.py

---
+++ uva540.py
@@ -0,0 +1,50 @@
+import queue
+
+def my_input():
+    try:
+        return input()
+    except:
+        return False
+
+def main():
+    task_no=0
+    while True:
+        q_n = int(my_input())
+        if int(q_n) == 0:
+            return
+
+        team = dict()
+        for i in range(q_n):
+            tmp = my_input()
+            tmp = tmp.split()
+            n = int(tmp[0])
+            for j in range(n):
+                team[tmp[j+1]] = i
+
+        task_no += 1
+        print("Scenario #%d" % task_no)
+        q = queue.Queue()
+        q2 = [queue.Queue() for _ in range(q_n)]
+        while True:
+            tmp = my_input()
+            if tmp == "STOP":
+                break
+            elif tmp == "DEQUEUE":
+                t = q.queue[0]
+                print(q2[t].get())
+
+                if q2[t].empty():
+                    q.get()
+
+            else:
+                oper, x = tmp.split()
+                t = team[x]
+
+                if q2[t].empty():
+                    q.put(t)
+
+                q2[t].put(x)
+
+        print("")
+
+main()
