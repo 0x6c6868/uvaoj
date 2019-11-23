caa47e0e354bfb5395ccc3a7d152b0f332b6b10e
0x6c6868 <1023347025@qq.com>
2019-11-23 22:35:48 +0800

    uva1583

---
+++ src/b1ch03/uva1583.java
@@ -0,0 +1,33 @@
+package b1ch03;
+
+import java.util.Scanner;
+
+public class uva1583 {
+
+    private static int[] ans = new int[100005];
+
+    private static void init() {
+        for (int i = 0; i < ans.length; i++) {
+
+            int sum = i, curr = i;
+            while (curr != 0) {
+                sum += curr % 10;
+                curr = curr / 10;
+            }
+
+            if (sum < ans.length && ans[sum] == 0) ans[sum] = i;
+        }
+    }
+
+    public static void main(String[] args) {
+        init();
+        Scanner sc = new Scanner(System.in);
+        int count = sc.nextInt();
+        while (count > 0) {
+            int num = sc.nextInt();
+            System.out.println(ans[num]);
+            count--;
+        }
+    }
+
+}
